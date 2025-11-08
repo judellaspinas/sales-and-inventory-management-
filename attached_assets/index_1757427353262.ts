6bd94051cd45ff1d86f8b2608db764a846270d31
judellaspinas <laspinasj4@gmail.com>
2025-11-08 19:46:17 +0800

    updated

--- attached_assets/index_1757427353262.ts
+++ attached_assets/index_1757427353262.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import session from "express-session";
 import MemoryStore from "memorystore";
-import { registerRoutes } from "../server/routes";
-import { setupVite, serveStatic, log } from "../server/vite";
+import { registerRoutes } from "./routes_1757427353264.js";
+import { setupVite, serveStatic, log } from "./vite_1757427353266.js";
 
 dotenv.config();
 
